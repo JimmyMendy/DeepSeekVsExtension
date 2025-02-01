6c3b43c1ca39e2587e28071cadf3dd95a088f3da
JimmyMendy <jimmy.mendy@gmail.com>
2025-02-01 08:31:42 +0000

    deepseek chat window

---
+++ src/extension.ts
@@ -0,0 +1,90 @@
+import * as vscode from "vscode";
+import ollama from "ollama";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("DsAssist is now active!");
+
+  const disposable = vscode.commands.registerCommand("deepC", () => {
+    const panel = vscode.window.createWebviewPanel(
+      "deepChat",
+      "Deep Seek Chat",
+      vscode.ViewColumn.One,
+      { enableScripts: true }
+    );
+
+    panel.webview.html = getWebviewContent();
+
+    panel.webview.onDidReceiveMessage(async (message: any) => {
+      if (message.command === "chat") {
+        const userPrompt = message.text;
+        let responseText = "";
+
+        try {
+          const streamResponse = await ollama.chat({
+            model: "deepseek-r1:latest",
+            messages: [{ role: "user", content: userPrompt }],
+            stream: true,
+          });
+
+          for await (const part of streamResponse) {
+            responseText += part.message.content;
+            panel.webview.postMessage({
+              command: "chatReponse",
+              text: responseText,
+            });
+          }
+        } catch (error) {
+          panel.webview.postMessage({
+            command: "chatResponse",
+            text: `Error: ${String(error)}`,
+          });
+        }
+      }
+    });
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(): string {
+  return /*html*/ `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<style>
+				body { font-family: sans-serif; margin: 1rem; }
+				#prompt { width: 100%; box-sizing: border-box; }
+				#response { border: 1px solid #ccc; margin-top: 1rem; padding: 0.5rem; min-height:}
+			</style>
+			<title>Document</title>
+		</head>
+		<body>
+			<h2>DeepSeek VSCode Extention</h2>
+			<textarea id="prompt" rows="3" placeholder="Ask something..."></textarea><br/>
+			<button id="askBtn">Ask</button>
+			<div id="response"></div>
+
+			<script>
+				const vscode = acquireVsCodeApi()
+
+				document.getElementById('askBtn').addEventListener("click", () => {
+					const text = document.getElementById('Prompt').value;
+					vscode.postMessage({ command: 'chat', text })
+				});
+
+				window.addEventListener('message', event => {
+					const { command, text } = event.data;
+					if (command === 'chatResponse') {
+						document.getElementById('response').innerText = text;
+					}
+				})
+			</script>
+		</body>
+		</html>
+	`;
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
